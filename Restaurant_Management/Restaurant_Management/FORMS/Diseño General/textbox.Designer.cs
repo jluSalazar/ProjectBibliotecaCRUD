e7f8d5fa4681963f3b113fb36dd95b9e624f42f6
Marlow Armijo <marlowarmijolopez@gmail.com>
2023-06-27 17:19:21 -0500

    cambio marlow

---
+++ Restaurant_Management/Restaurant_Management/FORMS/Diseño General/textbox.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ProyectoIB.Diseño_General
+{
+    partial class textbox
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(7, 7);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(236, 16);
+            textBox1.TabIndex = 0;
+            // 
+            // textbox
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            Controls.Add(textBox1);
+            Name = "textbox";
+            Padding = new Padding(7);
+            Size = new Size(250, 30);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
